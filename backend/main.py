894fcd3850c9813554c8e929265864a396eb6a32
FARHAN ABID <156011263+farhanabid786@users.noreply.github.com>
2026-03-08 13:46:37 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -30,3 +30,7 @@ async def predict_image(file: UploadFile = File(...)):
         "confidence": round(confidence * 100, 2)
     }
 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
+
