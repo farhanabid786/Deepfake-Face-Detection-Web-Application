e9b2c49a1dbb3550b625b2c437f6e197a6afc290
FARHAN ABID <156011263+farhanabid786@users.noreply.github.com>
2026-03-08 12:51:10 +0530

    Update model_loader.py

--- backend/model_loader.py
+++ backend/model_loader.py
@@ -1,7 +1,29 @@
-import tensorflow as tf
+# import tensorflow as tf
 
+# MODEL_PATH = "besttrainedmodel.h5"
+
+# model = tf.keras.models.load_model(MODEL_PATH)
+
+# def predict(img_tensor):
+#     prob = model.predict(img_tensor)[0][0]
+#     label = "Fake" if prob > 0.5 else "Real"
+#     confidence = prob if prob > 0.5 else 1 - prob
+#     return label, float(confidence
+
+import os
+import tensorflow as tf
+import urllib.request
+
+MODEL_URL = "https://drive.google.com/uc?export=download&id=16UMV3ATLGDiNUVvbgFGLEIJ2XguHnZ39"
 MODEL_PATH = "besttrainedmodel.h5"
 
+# download model if not present
+if not os.path.exists(MODEL_PATH):
+    print("Downloading model from Google Drive...")
+    urllib.request.urlretrieve(MODEL_URL, MODEL_PATH)
+    print("Model downloaded.")
+
+print("Loading model...")
 model = tf.keras.models.load_model(MODEL_PATH)
 
 def predict(img_tensor):
@@ -9,3 +31,4 @@ def predict(img_tensor):
     label = "Fake" if prob > 0.5 else "Real"
     confidence = prob if prob > 0.5 else 1 - prob
     return label, float(confidence)
+
