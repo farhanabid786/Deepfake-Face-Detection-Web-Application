5869d412e482740aae459eae029cc168b272a2fb
FARHAN ABID <156011263+farhanabid786@users.noreply.github.com>
2026-03-08 15:23:05 +0530

    Update download_model.py

--- backend/download_model.py
+++ backend/download_model.py
@@ -1,12 +1,31 @@
+# import gdown
+# import os
+
+# FILE_ID = "16UMV3ATLGDiNUVvbgFGLEIJ2XguHnZ39"
+# MODEL_PATH = "besttrainedmodel.h5"
+
+# if not os.path.exists(MODEL_PATH):
+#     print("Downloading model during build...")
+#     url = f"https://drive.google.com/uc?id={FILE_ID}"
+#     gdown.download(url, MODEL_PATH, quiet=False)
+# else:
+#     print("Model already exists")
+
+
 import gdown
 import os
 
 FILE_ID = "16UMV3ATLGDiNUVvbgFGLEIJ2XguHnZ39"
-MODEL_PATH = "besttrainedmodel.h5"
+MODEL_PATH = "besttrainedmodel.tflite"
 
 if not os.path.exists(MODEL_PATH):
+
     print("Downloading model during build...")
+
     url = f"https://drive.google.com/uc?id={FILE_ID}"
+
     gdown.download(url, MODEL_PATH, quiet=False)
+
 else:
+
     print("Model already exists")
