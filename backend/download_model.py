84c4a08f82c5a256ee9ca9631eb51d8f9ca7e90e
FARHAN ABID <156011263+farhanabid786@users.noreply.github.com>
2026-03-08 14:42:10 +0530

    Update download_model.py

--- backend/download_model.py
+++ backend/download_model.py
@@ -5,8 +5,8 @@ FILE_ID = "16UMV3ATLGDiNUVvbgFGLEIJ2XguHnZ39"
 MODEL_PATH = "besttrainedmodel.h5"
 
 if not os.path.exists(MODEL_PATH):
-print("Downloading model during build...")
-url = f"https://drive.google.com/uc?id={FILE_ID}"
-gdown.download(url, MODEL_PATH, quiet=False)
+    print("Downloading model during build...")
+    url = f"https://drive.google.com/uc?id={FILE_ID}"
+    gdown.download(url, MODEL_PATH, quiet=False)
 else:
-print("Model already exists")
+    print("Model already exists")
