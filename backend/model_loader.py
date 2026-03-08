74da24e05076ae741667458130dc26f3163bc81b
FARHAN ABID <156011263+farhanabid786@users.noreply.github.com>
2026-03-08 13:45:43 +0530

    Update model_loader.py

--- backend/model_loader.py
+++ backend/model_loader.py
@@ -8,23 +8,33 @@
 #     prob = model.predict(img_tensor)[0][0]
 #     label = "Fake" if prob > 0.5 else "Real"
 #     confidence = prob if prob > 0.5 else 1 - prob
-#     return label, float(confidence
-import tensorflow as tf
+#     return label, float(confidenceimport tensorflow as tf
 import os
 import gdown
 
 MODEL_PATH = "besttrainedmodel.h5"
 FILE_ID = "16UMV3ATLGDiNUVvbgFGLEIJ2XguHnZ39"
 
-if not os.path.exists(MODEL_PATH):
-    print("Downloading model from Google Drive...")
-    url = f"https://drive.google.com/uc?id={FILE_ID}"
-    gdown.download(url, MODEL_PATH, quiet=False)
+model = None
 
-print("Loading model...")
-model = tf.keras.models.load_model(MODEL_PATH)
+def load_model():
+    global model
+
+    if model is not None:
+        return model
+
+    if not os.path.exists(MODEL_PATH):
+        print("Downloading model from Google Drive...")
+        url = f"https://drive.google.com/uc?id={FILE_ID}"
+        gdown.download(url, MODEL_PATH, quiet=False)
+
+    print("Loading model...")
+    model = tf.keras.models.load_model(MODEL_PATH)
+    return model
+
 
 def predict(img_tensor):
+    model = load_model()
     prob = model.predict(img_tensor)[0][0]
     label = "Fake" if prob > 0.5 else "Real"
     confidence = prob if prob > 0.5 else 1 - prob
